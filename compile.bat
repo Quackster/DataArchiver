84395aa0d09b9c71e91a1746cb83b34691d2e6bb
Quackster <quacksterr.15@gmail.com>
2024-02-22 22:32:37 +1000

    Delete compile.bat [skip actions]

--- compile.bat
+++
@@ -1,3 +0,0 @@
-dotnet publish --self-contained -c Release -r ubuntu.16.10-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
-dotnet publish --self-contained -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
-pause
